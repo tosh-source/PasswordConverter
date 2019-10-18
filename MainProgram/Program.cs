495b234ee5f071db2f841ab609735e7b4b50646d
Todor Chupov -T (tchupov - Adecco at Cisco) <tchupov@cisco.com>
2019-10-18 12:21:30 +0300

    input settings and 2 empty methods

---
+++ MainProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input settings
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;  //independent from PC culture settings
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //core 
+            
+
+        }
+
+        private static void DecodeBase64(String inputText)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void EncodeBase64()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
